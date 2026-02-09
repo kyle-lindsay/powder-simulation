28610559ea964e5fae1c0d6c671b18a9e122e960
kyle <kyle@lindsay.gr>
2026-02-09 18:33:15 +0000

    Basic rendering set up

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Welcome to Powder Simulator");
-}   
+use macroquad::prelude::*;
+
+const WIDTH : u8 = 16;
+const HEIGHT : u8 = 16;
+
+#[macroquad::main("powder-sim")]
+async fn main() {
+    let window_width = screen_width();
+    let window_height = screen_height();
+
+    loop {
+        clear_background(BLUE);
+        // 1) update your state
+        // 2) draw the frame
+        next_frame().await;
+    }
+}
